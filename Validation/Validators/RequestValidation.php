59d426f8f619e277de27c9030c7f08cfd53cbe8c
Adeyemi Olaoye <yemi@cottacush.com>
2015-10-07 00:29:52 +0100

    add RequestValidation to validate request without creating classes

---
+++ Validation/Validators/RequestValidation.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Author: Adeyemi Olaoye <yemi@cottacush.com>
+ * Date: 07/10/15
+ * Time: 12:29 AM
+ */
+
+namespace PhalconUtils\Validation\Validators;
+
+
+use PhalconUtils\Validation\BaseValidation;
+
+class RequestValidation extends BaseValidation
+{
+
+    /**
+     * validations setups
+     * @author Adeyemi Olaoye <yemi@cottacush.com>
+     * @return mixed
+     */
+    function initialize()
+    {
+        // TODO: Implement initialize() method.
+    }
+}
