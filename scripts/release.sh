2d9f35134e85fe6e10355999ef809444dbd07f17
Adeyemi Olaoye <yemi@cottacush.com>
2015-10-15 12:08:21 +0100

    add slack update to release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -62,6 +62,15 @@ git push origin master
 
 git checkout develop
 
-echo "Release done"
+slack_update=`git diff  --no-ext-diff --unified=0 --exit-code -a --no-prefix ${latest_git_commit_id}..HEAD ${script_dir}/../CHANGELOG.md | egrep "^\+"`
+echo ${slack_update} >> ${script_dir}/slack_update.temp
+slack_update=`sed  's .  ' ${script_dir}/slack_update.temp`
+echo ${slack_update} > ${script_dir}/slack_update.temp
+slack_update=`tail -n +3 ${script_dir}/slack_update.temp`
+rm ${script_dir}/slack_update.temp
 
+PAYLOAD="payload={\"channel\": \"phalcon-utils\", \"username\": \"Phalcon Utils Release Bot\", \"text\": \"Phalcon Utils {$release_version} has been released \n\n${slack_update}\", \"icon_emoji\": \":rat:\"}";
+curl -s -S -X POST --data-urlencode "$PAYLOAD" https://hooks.slack.com/services/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
+
+echo "Release done"
 
