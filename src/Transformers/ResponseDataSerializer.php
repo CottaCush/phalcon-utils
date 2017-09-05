71eddb929971eaa80548fd2e215969a01d7ed6ce
Adeyemi Olaoye <yemi@cottacush.com>
2017-09-05 15:12:08 +0100

    Feature: Add response data serializer

---
+++ src/Transformers/ResponseDataSerializer.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace PhalconUtils\Transformer;
+
+use League\Fractal\Serializer\ArraySerializer;
+
+/**
+ * Class ResponseDataSerializer
+ * @author Adeyemi Olaoye <yemi@cottacush.com>
+ * @package App\Fractal
+ */
+class ResponseDataSerializer extends ArraySerializer
+{
+    public function collection($resourceKey, array $data)
+    {
+        if ($resourceKey == null) {
+            return $data;
+        }
+
+        return [$resourceKey ?: 'data' => $data];
+    }
+
+    public function item($resourceKey, array $data)
+    {
+        if ($resourceKey == null) {
+            return $data;
+        }
+
+        return [$resourceKey ?: 'data' => $data];
+    }
+
+    public function null()
+    {
+        return null;
+    }
+}
